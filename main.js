d1f82b853bf578fb6a148cef47af2dec315bf40b
Rocky Neurock <rocky.neurock@utopiamusic.com>
2024-02-03 20:40:29 +0100

    Add scale toggle

--- main.js
+++ main.js
@@ -2,21 +2,29 @@ const CALC_BUTTON_CLASS = 'calculator__button';
 const CALC_CARBS_SELECTOR = '#carbs';
 const CALC_GLUCOSE_SELECTOR = '#glucose';
 const CALC_UNITS_SELECTOR = '.calculator__units';
+
 const DOSE_CEILING_THRESHOLD = 0.3;
+
 const PREFERENCE_KEY_CARBS = 'carbs';
 const PREFERENCE_KEY_CARBS_DOSAGE = 'carbs-dosage';
 const PREFERENCE_KEY_DOSAGE = 'dosage';
 const PREFERENCE_KEY_INCREMENT = 'increment';
+const PREFERENCE_KEY_SCALE = 'scale';
 const PREFERENCE_KEY_THRESHOLD = 'threshold';
-const PREFERENCES_BUTTON_SELECTOR = '.preferences button';
-const PREFERENCES_CARBS_DOSAGE_SELECTOR = '#preferences__carbs-dosage';
-const PREFERENCES_CARBS_SELECTOR = '#preferences__carbs';
-const PREFERENCES_DOSAGE_SELECTOR = '#preferences__dosage';
-const PREFERENCES_FORM_SELECTOR = '.preferences';
-const PREFERENCES_INCREMENT_SELECTOR = '#preferences__increment';
-const PREFERENCES_THRESHOLD_SELECTOR = '#preferences__threshold';
-const PREFERENCES_MESSAGE_SELECTOR = '.preferences__message';
+
+const PREFERENCES_DEFAULT_SCALE = 'mg';
+
 const PREFERENCES_MESSAGE_TIMEOUT = 1800;
+
+const PREFERENCES_SELECTOR_BUTTON = '.preferences button';
+const PREFERENCES_SELECTOR_CARBS_DOSAGE = '#preferences__carbs-dosage';
+const PREFERENCES_SELECTOR_CARBS = '#preferences__carbs';
+const PREFERENCES_SELECTOR_DOSAGE = '#preferences__dosage';
+const PREFERENCES_SELECTOR_FORM = '.preferences';
+const PREFERENCES_SELECTOR_INCREMENT = '#preferences__increment';
+const PREFERENCES_SELECTOR_SCALE = '#scale';
+const PREFERENCES_SELECTOR_THRESHOLD = '#preferences__threshold';
+const PREFERENCES_SELECTOR_MESSAGE = '.preferences__message';
 
 const GLUCOSE_MODIFIER = {
   'down': -75,
@@ -31,8 +39,14 @@ const PREFERENCES = {
   carbsDosage: '',
   dosage: '',
   increment: '',
+  scale: PREFERENCES_DEFAULT_SCALE,
   threshold: '',
 }
+
+const SCALE_MODIFIER = {
+  mg: 1,
+  mm: 18
+};
 
 let savePreferencesMessage;
 
@@ -51,7 +65,7 @@ function calculateDose({ carbs, glucose }) {
 }
 
 function disableSavePreferences() {
-  document.querySelector(PREFERENCES_BUTTON_SELECTOR).setAttribute('disabled', true);
+  document.querySelector(PREFERENCES_SELECTOR_BUTTON).setAttribute('disabled', true);
 }
 
 function displaySavePreferencesMessage(message, callback) {
@@ -67,26 +81,37 @@ function displaySavePreferencesMessage(message, callback) {
 }
 
 function enableSavePreferences() {
-  document.querySelector(PREFERENCES_BUTTON_SELECTOR).removeAttribute('disabled');
+  document.querySelector(PREFERENCES_SELECTOR_BUTTON).removeAttribute('disabled');
 }
 
 function loadPreferences() {
   savePreferences();
 
   if (PREFERENCES.carbs)
-    document.querySelector(PREFERENCES_CARBS_SELECTOR).value = PREFERENCES.carbs;
+    document.querySelector(PREFERENCES_SELECTOR_CARBS).value = PREFERENCES.carbs;
 
   if (PREFERENCES.carbsDosage)
-    document.querySelector(PREFERENCES_CARBS_DOSAGE_SELECTOR).value = PREFERENCES.carbsDosage;
+    document.querySelector(PREFERENCES_SELECTOR_CARBS_DOSAGE).value = PREFERENCES.carbsDosage;
 
   if (PREFERENCES.dosage)
-    document.querySelector(PREFERENCES_DOSAGE_SELECTOR).value = PREFERENCES.dosage;
+    document.querySelector(PREFERENCES_SELECTOR_DOSAGE).value = PREFERENCES.dosage;
 
   if (PREFERENCES.increment)
-    document.querySelector(PREFERENCES_INCREMENT_SELECTOR).value = PREFERENCES.increment;
+    document.querySelector(PREFERENCES_SELECTOR_INCREMENT).value = PREFERENCES.increment;
+
+  if (PREFERENCES.scale) {
+    document.querySelector(PREFERENCES_SELECTOR_SCALE).value = PREFERENCES.scale;
+    document
+      .querySelectorAll(`${PREFERENCES_SELECTOR_SCALE} option`)
+      .forEach(function(option) {
+        if (option.value === PREFERENCES.scale) {
+          option.toggleAttribute('selected');
+        }
+      });
+  }
 
   if (PREFERENCES.threshold)
-    document.querySelector(PREFERENCES_THRESHOLD_SELECTOR).value = PREFERENCES.threshold;
+    document.querySelector(PREFERENCES_SELECTOR_THRESHOLD).value = PREFERENCES.threshold;
 }
 
 function roundDose(dose) {
@@ -102,6 +127,7 @@ function savePreferences() {
   PREFERENCES.carbsDosage = localStorage.getItem(PREFERENCE_KEY_CARBS_DOSAGE);
   PREFERENCES.dosage = localStorage.getItem(PREFERENCE_KEY_DOSAGE);
   PREFERENCES.increment = localStorage.getItem(PREFERENCE_KEY_INCREMENT);
+  PREFERENCES.scale = localStorage.getItem(PREFERENCE_KEY_SCALE);
   PREFERENCES.threshold = localStorage.getItem(PREFERENCE_KEY_THRESHOLD);
 }
 
@@ -109,7 +135,8 @@ function setUpCalculatorButtonHandler() {
   document.addEventListener('click', function(e) {
     if (e.target.className.includes(CALC_BUTTON_CLASS)) {
       let glucoseInput = document.querySelector(CALC_GLUCOSE_SELECTOR).value;
-      let glucose = glucoseInput && parseFloat(glucoseInput) * 18 || 0;
+      let scaleModifier = SCALE_MODIFIER[PREFERENCES.scale];
+      let glucose = glucoseInput && parseFloat(glucoseInput) * scaleModifier || 0;
       let carbsInput = document.querySelector(CALC_CARBS_SELECTOR).value;
       let carbs = carbsInput && parseInt(carbsInput) || 0;
 
@@ -122,8 +149,18 @@ function setUpCalculatorButtonHandler() {
   });
 }
 
+function setUpScaleChangeHandler() {
+  document.querySelector(PREFERENCES_SELECTOR_SCALE).addEventListener('change', function(e) {
+    let scale = e.target.value;
+
+    localStorage.setItem(PREFERENCE_KEY_SCALE, scale);
+
+    savePreferences();
+  });
+}
+
 function setUpPreferenceFormHandler() {
-  document.querySelector(PREFERENCES_FORM_SELECTOR).addEventListener('submit', function(e) {
+  document.querySelector(PREFERENCES_SELECTOR_FORM).addEventListener('submit', function(e) {
     e.preventDefault();
 
     try {
@@ -149,10 +186,12 @@ function setUpPreferenceFormHandler() {
 (function main() {
   document.addEventListener('readystatechange', function() {
     if (document.readyState === 'complete') {
-      savePreferencesMessage = document.querySelector(PREFERENCES_MESSAGE_SELECTOR);
+      savePreferencesMessage = document.querySelector(PREFERENCES_SELECTOR_MESSAGE);
 
       setUpCalculatorButtonHandler();
       setUpPreferenceFormHandler();
+      setUpScaleChangeHandler();
+
       loadPreferences();
     }
   });
